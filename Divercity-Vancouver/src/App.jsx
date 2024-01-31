762c6a8ad86a94183ef34824e990e3a3dff77053
Dan <notnow0388@gmail.com>
2024-01-30 20:07:17 -0800

    basic structure

---
+++ Divercity-Vancouver/src/App.jsx
@@ -0,0 +1,14 @@
+import Homepage from './Homepage/homepage.jsx'
+import React from 'react'
+
+function App() {
+ 
+  return (
+    <div>
+    <Homepage />    
+
+  </div>
+      )
+}
+
+export default App
