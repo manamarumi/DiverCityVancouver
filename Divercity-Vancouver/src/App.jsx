06e005a0ce0d8a843fd13a7b5eb2eb7da84d646e
raynahuang <raynainca190217@gmail.com>
2024-02-06 23:23:44 -0800

    make a sample of the Sign up page

--- Divercity-Vancouver/src/App.jsx
+++ Divercity-Vancouver/src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {BrowserRouter  as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/homepage';
-import Calander from './pages/Calendar/calendar';
+import Calendar from './pages/Calendar/calendar';
 import Adminpage from './pages/Admin/adminpage';
-import Postevents from './pages/Admin/adminpage';
-import Editevents from './pages/Admin/adminpage';
-import Postnews from './pages/Admin/adminpage';
-import Editnews from './pages/Admin/adminpage';
-
+import Postevents from './pages/Admin/postevents'; // Corrected import
+import Editevents from './pages/Admin/editevents'; // Corrected import
+import Postnews from './pages/Admin/postnews'; // Corrected import
+import Editnews from './pages/Admin/editnews'; // Corrected import
+import Signuppage from './pages/Signuppage/signuppage'; // Corrected import
 
 function App() {
   return (
     <div>
       <Router>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<Homepage />} />
-            <Route path="/signup" element={<Homepage />} />
-            <Route path="/calendar" element={<Calander />} />
-            <Route path="/events" element={<Homepage />} />
-            <Route path="/admin" element={<Adminpage />} />   
-            <Route path="/admin/postevent" element={<Postevents />} />
-            <Route path="/admin/editevent" element={<Editevents />} />
-            <Route path="/admin/postnews" element={<Postnews />} />   
-            <Route path="/admin/editnews" element={<Editnews />} />       
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Homepage />} />
+          <Route path="/signup" element={<Signuppage />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/events" element={<Homepage />} />
+          <Route path="/admin" element={<Adminpage />} />   
+          <Route path="/admin/postevent" element={<Postevents />} />
+          <Route path="/admin/editevent" element={<Editevents />} />
+          <Route path="/admin/postnews" element={<Postnews />} />   
+          <Route path="/admin/editnews" element={<Editnews />} />       
+        </Routes>
       </Router>
     </div>
   );
