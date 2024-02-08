1304a920c88714dc888ba03b043dc349049d1ba1
Prabhjyot <knowprabhjyot@gmail.com>
2024-02-07 17:44:50 -0800

    Firebase Signup Working

--- Divercity-Vancouver/src/pages/Customer/Signuppage/signuppage.jsx
+++ Divercity-Vancouver/src/pages/Customer/Signuppage/signuppage.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import signupImage from '../../../assets/signuppics/signupImage.jpg';
 import googleIcon from '../../../assets/signuppics/googleIcon.png'; // Import the Google icon image
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../../firebase';
 
 const Signuppage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSignup = (event) => {
+
+    event.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+
+        if (user) {
+          alert('User Registered successfully');
+          navigate('/login');
+        }
+
+        // save the name in the Firebase collection for this particular user
+        // {YOUR CODE SHOULD GO HERE}
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+
+        alert(errorMessage);
+      });
+  }
+
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex h-[550px] w-[1100px] justify-between bg-white rounded-xl overflow-hidden" style={{ boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px' }}>
@@ -23,15 +56,15 @@ const Signuppage = () => {
                 <label className="block text-sm font-medium text-gray-700" htmlFor="email">
                   Email:
                 </label>
-                <Input className="w-full" id="email" placeholder="Your email" type="email" />
+                <Input value={email} onChange={(event) => setEmail(event.target.value)}  className="w-full" id="email" placeholder="Your email" type="email" />
               </div>
               <div>
                 <label className="block text-sm font-medium text-gray-700" htmlFor="password">
                   Password:
                 </label>
-                <Input className="w-full" id="password" placeholder="Your password" type="password" />
+                <Input value={password} onChange={(event) => setPassword(event.target.value)} className="w-full" id="password" placeholder="Your password" type="password" />
               </div>
-              <Button className="w-full" style={{ backgroundColor: '#4654A3' }}>Sign up</Button>
+              <Button  onClick={handleSignup}  className="w-full" style={{ backgroundColor: '#4654A3' }}>Sign up</Button>
             </form>
             <p className="text-sm text-gray-600">
               Already have an account? <Link to="/login" className="text-blue-600">Login now</Link>
